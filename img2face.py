b50c5f88beddec9a38975296cb0f9585c638c943
Pablo Barrios <pablo_barrios3@hotmail.com>
2016-04-09 14:45:40 -0700

    momo

--- img2face.py
+++ img2face.py
@@ -2,6 +2,21 @@
 
 import numpy as np
 import cv2
+import sys
 
 # Load an color image in grayscale
-img = cv2.imread('sample.JPG',0)
+img = cv2.imread('Momo.jpg', 1)
+cv2.namedWindow('Momo.jpg', cv2.WINDOW_NORMAL)
+#cv2.imshow('Momo.jpg', img)
+r = 1500.0 / img.shape[1]
+dim = (1500, int(img.shape[0] * r))
+
+# perform the actual resizing of the image and show it
+resized = cv2.resize(img, dim, interpolation = cv2.INTER_AREA)
+cv2.imshow("resized", resized)
+
+
+
+
+cv2.waitKey(0)
+cv2.destroyAllWindows()
